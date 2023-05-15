5356fc1ebc8faa386b21590c97b24ccae8873de9
Abraão <avraamisvi@users.noreply.github.com>
2023-05-15 08:28:38 +0200

    first commit

---
+++ core/src/com/postmilgames/thetower/Layer.java
@@ -0,0 +1,12 @@
+package com.postmilgames.thetower;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+public class Layer extends Actor {
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+}
---
+++ core/src/com/postmilgames/thetower/Map.java
@@ -0,0 +1,4 @@
+package com.postmilgames.thetower;
+
+public class Map {
+}
